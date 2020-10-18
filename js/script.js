c851709747260562081478af4e72cbb24a7e5118
Sebastian Faryna <sebastianfaryna@gmail.com>
2020-10-18 17:29:48 +0100

    Add another conditions if/else in JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+// Ruch Gracza //
+
+let playerMove = 'nieznany ruch';
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+} else if (playerInput == '2'){
+    playerMove = 'papier';
+} else if (playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+// Ruch komputera //
+
+let computerMove = 'nieznany ruch';
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
     computerMove = 'kamień';
@@ -14,18 +32,18 @@ if(randomNumber == 1){
 
 printMessage('Mój ruch to: ' + computerMove);
 
-let playerInput = prompt('Wybierz Twój ruch! 1: kamień, 2: papier, 3: nożyce.');
+// Wynik gry //
 
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-    playerMove = 'kamień';
-} else if (playerInput == '2'){
-    playerMove = 'papier';
-} else if (playerInput == '3'){
-    playerMove = 'nożyce';
-}
-
-printMessage('Twój ruch to: ' + playerMove);
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Wygrałeś!');
+  } else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Wygrałeś!');
+  } else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Wygrałeś!');
+  } else if (computerMove == playerMove){
+    printMessage('Remis!');
+  } else if (playerMove == 'nieznany ruch'){
+    printMessage('Remis!');
+  } else {
+    printMessage('Przegrałeś!');
+  }
