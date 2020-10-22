8aa960fd444f1882ece9a3b1843761d27a6b1660
Sebastian Faryna <sebastianfaryna@gmail.com>
2020-10-22 10:27:41 +0100

    Add points JS script and change language

--- js/script.js
+++ js/script.js
@@ -1,45 +1,49 @@
+    // Punkty //
+let playerScore = 0;
+
 {
 const playGame = function(playerInput){
     clearMessages();
     const getMoveName = function(argMoveId){
         if(argMoveId == 1){
-            return 'kamień';
+            return 'rock';
         } else if(argMoveId == 2){
-            return 'papier';
+            return 'paper';
         } else if(argMoveId == 3){
-            return 'nożyce';
+            return 'scissors';
         }
     }
 
     // Ruch komputera //
 const randomNumber = Math.floor(Math.random() * 3 + 1);
 const computerMove = getMoveName(randomNumber);
-console.log('Wylosowana liczba to: ' + randomNumber);
-printMessage('Mój ruch to: ' + computerMove);
+console.log('The drawn number is: ' + randomNumber);
+printMessage('My move is: ' + computerMove);
 
     // Ruch Gracza //
 const playerMove = getMoveName(playerInput);
-console.log('Gracz wpisał: ' + playerInput);
-printMessage('Twój ruch to: ' + playerMove);
+console.log('Player input: ' + playerInput);
+printMessage('Your move is: ' + playerMove);
 
     // Wynik gry //
 const argComputerMove = getMoveName(randomNumber);
 const argPlayerMove = getMoveName(playerInput);
 const displayResult = function(argComputerMove, argPlayerMove){
 console.log('Ruchy to:', argComputerMove, argPlayerMove);
-    if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-            printMessage('Wygrałeś!');
-        } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-            printMessage('Wygrałeś!');
-        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
-            printMessage('Wygrałeś!');
+    if(argComputerMove == 'rock' && argPlayerMove == 'paper'){
+            printMessage("You've won! Point for you!"); playerScore = playerScore + 1; console.log('Score = ' + playerScore);
+        } else if (argComputerMove == 'rock' && argPlayerMove == 'scissors'){
+            printMessage('You score!'); playerScore = playerScore + 1; console.log('Score = ' + playerScore);
+        } else if (argComputerMove == 'scissors' && argPlayerMove == 'rock'){
+            printMessage('Nice!'); playerScore = playerScore + 1; console.log('Score = ' + playerScore);
         } else if (argComputerMove == argPlayerMove){
-            printMessage('Remis!');
+            printMessage('DRAW!'); console.log('Remis. Punkty = ' + playerScore);
         } else {
-            printMessage('Przegrałeś!');
+            printMessage('YOU LOST!'), playerScore = 0; console.log('Score reset = ' + playerScore);
         }
     }
-    displayResult(argComputerMove, argPlayerMove)
+    displayResult(argComputerMove, argPlayerMove);
+    printMessage('Scores: ' + playerScore)
 }
 
     // Buttons //
