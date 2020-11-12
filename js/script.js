0b5c4d9c32b6a6112471d1ee30c9ff9beccca231
Sebastian Faryna <sebastianfaryna@gmail.com>
2020-11-12 13:00:55 +0100

    Clear the  JS code

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
     // Punkty //
     let playerScore = 0;
 
-    const playGame = function (playerInput) {
+    const playGame = function(playerInput) {
         clearMessages();
-        const getMoveName = function (argMoveId) {
+        const getMoveName = function(argMoveId) {
             if (argMoveId == 1) {
                 return 'rock';
             } else if (argMoveId == 2) {
@@ -19,70 +19,60 @@
         }
 
         // Ruch Gracza //
-        const playerMove = getMoveName(playerInput);
-        console.log('Player input: ' + playerInput);
-        printMessage('Your move is: ' + playerMove);
+        const argPlayerMove = getMoveName(playerInput);
+        printMessage('Your move is: ' + argPlayerMove);
 
         // Ruch komputera //
         const randomNumber = Math.floor(Math.random() * 5 + 1);
-        const computerMove = getMoveName(randomNumber);
-        console.log('The drawn number is: ' + randomNumber);
-        printMessage('My move is: ' + computerMove);
+        const argComputerMove = getMoveName(randomNumber);
+        printMessage('My move is: ' + argComputerMove);
 
         // Wynik gry //
-        const displayResult = function (argcomputerMove, argplayerMove) { // nazwy tych argumentów nie muszą być takie same jak zmiennych/stałych
-            console.log('Moves: ', computerMove, playerMove);
-            if ((computerMove == 'rock' && playerMove == 'paper') || (computerMove == 'rock' && playerMove == 'Spock')) {
+        const displayResult = function(argComputerMove, argPlayerMove) {
+            if ((argComputerMove == 'rock' && argPlayerMove == 'paper') || (argComputerMove == 'rock' && argPlayerMove == 'Spock')) {
                 printMessage("You've won! Point for you!");
                 playerScore++;
-                console.log('Points = ' + playerScore);
-            } else if ((computerMove == 'paper' && playerMove == 'scissors') || (computerMove == 'paper' && playerMove == 'lizard')) {
+            } else if ((argComputerMove == 'paper' && argPlayerMove == 'scissors') || (argComputerMove == 'paper' && argPlayerMove == 'lizard')) {
                 printMessage('You score!');
                 playerScore++;
-                console.log('Points = ' + playerScore);
-            } else if ((computerMove == 'scissors' && playerMove == 'rock') || (computerMove == 'scissors' && playerMove == 'Spock')) {
+            } else if ((argComputerMove == 'scissors' && argPlayerMove == 'rock') || (argComputerMove == 'scissors' && argPlayerMove == 'Spock')) {
                 printMessage('Good job! +1 for You');
                 playerScore++;
-                console.log('Score = ' + playerScore);
-            } else if ((computerMove == 'Spock' && playerMove == 'paper') || (computerMove == 'Spock' && playerMove == 'lizard')) {
+            } else if ((argComputerMove == 'Spock' && argPlayerMove == 'paper') || (argComputerMove == 'Spock' && argPlayerMove == 'lizard')) {
                 printMessage('Way to go!');
                 playerScore++;
-                console.log('Score = ' + playerScore);
-            } else if ((computerMove == 'lizard' && playerMove == 'rock') || (computerMove == 'lizard' && playerMove == 'scissors')) {
+            } else if ((argComputerMove == 'lizard' && argPlayerMove == 'rock') || (argComputerMove == 'lizard' && argPlayerMove == 'scissors')) {
                 printMessage('Keep it up!');
                 playerScore++;
-                console.log('Score = ' + playerScore);
-            } else if (computerMove == playerMove) {
+            } else if (argComputerMove == argPlayerMove) {
                 printMessage('DRAW! Try again!');
-                console.log('Draw... (' + playerScore + ')');
             } else {
                 printMessage('YOU LOST!');
                 playerScore = 0;
-                console.log('Reset = ' + playerScore);
             }
         }
-        displayResult(computerMove, playerMove); // ważne żeby kolejność deklaracji argumentów się zgadzała
+        displayResult(argComputerMove, argPlayerMove);
         printMessage("Player's Scores: " + playerScore)
     }
 
     // Buttons //
-    document.getElementById('play-rock').addEventListener('click', function () {
+    document.getElementById('play-rock').addEventListener('click', function() {
         playGame(1);
     });
 
-    document.getElementById('play-paper').addEventListener('click', function () {
+    document.getElementById('play-paper').addEventListener('click', function() {
         playGame(2);
     });
 
-    document.getElementById('play-scissors').addEventListener('click', function () {
+    document.getElementById('play-scissors').addEventListener('click', function() {
         playGame(3);
     });
 
-    document.getElementById('play-lizard').addEventListener('click', function () {
+    document.getElementById('play-lizard').addEventListener('click', function() {
         playGame(4);
     });
 
-    document.getElementById('play-spock').addEventListener('click', function () {
+    document.getElementById('play-spock').addEventListener('click', function() {
         playGame(5);
     });
 }
